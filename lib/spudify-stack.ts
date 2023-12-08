9de86ab9e29e632d6e831002e6ec68abdd44e20f
Bella Lafrenais <bella.lafrenais@postcodelottery.co.uk>
2023-12-08 13:25:19 +0000

    add hello world lambda

--- lib/spudify-stack.ts
+++ lib/spudify-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import { aws_lambda as lambda } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -6,11 +7,10 @@ export class SpudifyStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'SpudifyQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const spudifyFunction = new lambda.Function(this, 'SpudifyFn', {
+      code: lambda.Code.fromAsset('./app/lambda/'),
+      runtime: lambda.Runtime.PYTHON_3_11,
+      handler:'spudifyFn.handler'
+    });
   }
 }
