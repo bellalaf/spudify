ef4b6c8682b4f9fcf1d6d191adaac733be4262e9
Bella Lafrenais <bella.lafrenais@postcodelottery.co.uk>
2023-12-08 13:24:58 +0000

    configure aws account and cdk action

--- bin/spudify.ts
+++ bin/spudify.ts
@@ -15,7 +15,7 @@ new SpudifyStack(app, 'SpudifyStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: '107263287233', region: 'eu-west-1' },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
